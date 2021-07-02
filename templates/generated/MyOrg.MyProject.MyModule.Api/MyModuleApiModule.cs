5f5c64834d0b3bddd36389770ede6bd2cbd307d9
Lulus <snys98@outlook.com>
2021-07-02 11:38:08 +0800

    忽略模板

--- templates/generated/MyOrg.MyProject.MyModule.Api/MyModuleApiModule.cs
+++
@@ -1,9 +0,0 @@
-using Geex.Common.Abstractions;
-using Volo.Abp.Modularity;
-
-namespace MyOrg.MyProject.MyModule.Api
-{
-    public class MyProjectMyModuleApiModule : GeexModule<MyProjectMyModuleApiModule>
-    {
-    }
-}
--- templates/generated/MyOrg.MyProject.MyModule.Core/MyModuleApiModule.cs
+++
@@ -1,9 +0,0 @@
-using Geex.Common.Abstractions;
-using Volo.Abp.Modularity;
-
-namespace MyOrg.MyProject.MyModule.Core
-{
-    public class MyProjectMyModuleCoreModule : GeexModule<MyProjectMyModuleCoreModule>
-    {
-    }
-}
