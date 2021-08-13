8fd651819ae54090f1fa2b28beb7ff389c044fa2
Lulus <snys98@outlook.com>
2021-08-13 16:42:41 +0800

    增加测试项目

---
+++ Geex.TemplateGenerator/templates/_org_._proj_._mod_.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _org_._proj_._mod_.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
